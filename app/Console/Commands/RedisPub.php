5f4cf11f14416515a644b888b608a77187f41dac
E.Wolf <e.wolf@webmi.ru>
2021-08-25 20:24:16 +0300

    Redis pub/sub example

---
+++ app/Console/Commands/RedisPub.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class RedisPub extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'redis:pub';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        Redis::publish('channel-1', json_encode([
+            'hello' => 'world 123',
+        ]));
+
+        return 0;
+    }
+}
